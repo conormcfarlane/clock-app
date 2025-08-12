81f6bacd4adf8f84ac96238e4d902a66a072fd06
conormcfarlane <conormcf98@gmail.com>
2025-08-12 19:36:41 +1000

    functionality almost all done just design and responsivness left

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
-
-export default function Button() {
+import iconArrowDown from "../assets/images/desktop/icon-arrow-down.svg"
+import iconArrowUp from "../assets/images/desktop/icon-arrow-up.svg"
+export default function Button({isMenuOpen,MenuToggle}) {
   return (
-    <div>Button</div>
-  )
+    <button className='flex items-center bg-white p-2 pl-5 rounded-full justify-between gap-3 w-35'>
+      <p className='font-semibold'>{!isMenuOpen ? "M O R E" : "L E S S"}</p>
+      <div className=' rounded-full flex items-center justify-center '>
+        <img src={iconArrowUp} alt="menu toggle button" onClick={MenuToggle} className={`w-10 h-10 ${!isMenuOpen ? "rotate-180" : "rotate-0"}`} />
+      </div>
+      
+    </button>
+  ) 
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { greetingHelper, isDaytimeHelper } from "../utils/timeHelper";
 import iconSun from "../assets/images/desktop/icon-sun.svg";
 import iconMoon from "../assets/images/desktop/icon-moon.svg";
 import { useTimeLocation } from "../context/timeLocationContext";
+import Button from "./Button";
 export default function Hero() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const MenuToggle = () => {
+    setIsMenuOpen((prev) => !prev);
+  };
   const { data, loading, error } = useTimeLocation();
   // Handle loading and error states
   if (loading) return <div>Loading...</div>;
@@ -13,17 +19,51 @@ export default function Hero() {
   // NOW use the API time data
   const apiTime = new Date(data.datetime);
   const currentHour = apiTime.getHours();
+  const currentMins = apiTime.getMinutes();
+  const currentTime = currentHour + ":" + currentMins;
   const greeting = greetingHelper(currentHour);
   const isDayTime = isDaytimeHelper(currentHour);
   const userCity = data.timezone.split("/").pop();
+  const userCountry = data.timezone.split("/").shift();
+  const abbrev = data.abbreviation;
 
+  const menuItems = [
+    { label: "CURRENT TIMEZONE", value: data?.timezone },
+    { label: "DAY OF THE YEAR", value: data?.day_of_year },
+    { label: "DAY OF THE WEEK", value: data?.day_of_week },
+    { label: "WEEK NUMBER", value: data?.week_number },
+  ];
   return (
-    <section className="bg-red-500">
-      <div className="flex gap-4">
-        <img src={isDayTime ? iconSun : iconMoon} alt="" />
-        <p className="text-xl">{greeting}</p>
-      </div>
-      <p>{userCity}</p>
-    </section>
+    <>
+      <section className="bg-red-500">
+        <div>
+          <div className="flex gap-4">
+            <img src={isDayTime ? iconSun : iconMoon} alt="" />
+            <p className="text-xl">{greeting}</p>
+          </div>
+          <p className="text-7xl">
+            {currentTime}
+            <span className="text-lg">{abbrev}</span>
+          </p>
+          <p>
+            In {userCity} , {userCountry}
+          </p>
+        </div>
+        <Button isMenuOpen={isMenuOpen} MenuToggle={MenuToggle} />
+      </section>
+      {isMenuOpen && (
+        <section className="bg-blue-900 py-10">
+          <div className="flex flex-col gap-4">
+            {menuItems.map((item,index) => (
+            <div key={index} className="flex justify-between">
+              <p>{item.label}</p>
+              <p className="font-semibold">{item.value}</p>
+            </div>
+          ))}
+          </div>
+          
+        </section>
+      )}
+    </>
   );
 }
